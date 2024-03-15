2614575e766ebcdcb7b4faabd30c06068cb091d3
imonaar <kevinmonari22@gmail.com>
2024-03-15 14:24:35 +0300

    v1 - single thread

---
+++ src/main.rs
@@ -0,0 +1,199 @@
+#![allow(unused)]
+
+use svg::node::element::path::Command;
+use svg::node::element::path::Data;
+use svg::node::element::path::Position;
+use svg::node::element::Path;
+use svg::node::element::Rectangle;
+use svg::Document;
+
+use crate::Operation::*;
+use crate::Orientation::*;
+
+/**
+ * HEIGHT and WIDTH providehe bounds of the drawing.
+ */
+const WIDTH: isize = 400;
+const HEIGHT: isize = WIDTH;
+
+/*
+   HOME_Y and HOME_X constants allow us to easily reset where we are
+   drawing from. Here y is the vertical coordinate and x is the horizontal.
+*/
+const HOME_Y: isize = HEIGHT / 2;
+const HOME_X: isize = WIDTH / 2;
+const STROKE_WIDTH: usize = 5;
+
+#[derive(Debug, Clone, Copy)]
+enum Operation {
+    Forwad(isize),
+    TurnLeft,
+    TurnRight,
+    Home,
+    Noop(u8),
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Orientation {
+    North,
+    East,
+    West,
+    South,
+}
+/**
+ * Artist maintains the state of the diagram
+ * Conceptualy the artist is holding the pen at the coordinates x, y and is moving in the direction of heading
+ */
+#[derive(Debug)]
+struct Artist {
+    x: isize,
+    y: isize,
+    heading: Orientation,
+}
+
+impl Artist {
+    fn new() -> Self {
+        Self {
+            heading: North,
+            x: HOME_X,
+            y: HOME_Y,
+        }
+    }
+
+    fn home(&mut self) {
+        self.x = HOME_X;
+        self.y = HOME_Y;
+    }
+
+    fn forward(&mut self, distance: isize) {
+        match self.heading {
+            North => self.y += distance,
+            South => self.y -= distance,
+            West => self.x += distance,
+            East => self.x -= distance,
+        };
+    }
+
+    fn turn_right(&mut self) {
+        self.heading = match self.heading {
+            North => East,
+            East => South,
+            South => West,
+            West => North,
+        }
+    }
+
+    fn turn_left(&mut self) {
+        self.heading = match self.heading {
+            North => West,
+            West => South,
+            South => East,
+            East => North,
+        }
+    }
+
+    fn wrap(&mut self) {
+        if self.x < 0 {
+            self.x = HOME_X;
+            self.heading = West;
+        } else if self.x > WIDTH {
+            self.x = HOME_X;
+            self.heading = East;
+        }
+
+        if self.y < 0 {
+            self.y = HOME_Y;
+            self.heading = North;
+        } else if self.y > HEIGHT {
+            self.y = HOME_Y;
+            self.heading = South;
+        }
+    }
+}
+
+fn parse(input: &str) -> Vec<Operation> {
+    let mut steps = Vec::<Operation>::new();
+    for byte in input.as_bytes() {
+        let step = match byte {
+            b'0' => Home,
+            b'1'..=b'9' => {
+                let distance = (byte - 0x30) as isize;
+                Forwad(distance * (HEIGHT / 10))
+            }
+            b'a' | b'b' | b'c' => TurnLeft,
+            b'd' | b'e' | b'f' => TurnRight,
+            _ => Noop(*byte),
+        };
+
+        steps.push(step)
+    }
+    steps
+}
+
+fn convert(operations: &Vec<Operation>) -> Vec<Command> {
+    let mut turtle = Artist::new();
+    let mut path_data: Vec<Command> = vec![];
+
+    let start_at_home = Command::Move(Position::Absolute, (HOME_X, HOME_Y).into());
+    path_data.push(start_at_home);
+
+    for op in operations {
+        match *op {
+            Forwad(distance) => turtle.forward(distance),
+            TurnLeft => turtle.turn_left(),
+            TurnRight => turtle.turn_right(),
+            Home => turtle.home(),
+            Noop(byte) => eprintln!("warning: illegal byte encountered: {:?}", byte),
+        }
+
+        let line = Command::Line(Position::Absolute, (turtle.x, turtle.y).into());
+        path_data.push(line);
+        turtle.wrap()
+    }
+    path_data
+}
+
+fn generate_svg(path_data: Vec<Command>) -> Document {
+    let background = Rectangle::new()
+        .set("x", 0)
+        .set("y", 0)
+        .set("width", WIDTH)
+        .set("height", HEIGHT)
+        .set("fill", "#fffff");
+
+    let border = background
+        .clone()
+        .set("fill-opacity", "0.0")
+        .set("stroke", "#ccccc")
+        .set("stroke-width", 3 * STROKE_WIDTH);
+
+    let sketch = Path::new()
+        .set("fill", "none")
+        .set("stroke", "#2f2f2f")
+        .set("stroke-width", STROKE_WIDTH)
+        .set("stroke-opacity", "0.9")
+        .set("d", Data::from(path_data));
+
+    let document = Document::new()
+        .set("ViewBox", (0, 0, HEIGHT, WIDTH))
+        .set("height", HEIGHT)
+        .set("width", WIDTH)
+        .set("style", "style=\"outline: 5px solid #800000;\"")
+        .add(background)
+        .add(sketch)
+        .add(border);
+
+    document
+}
+
+fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+    let input = args.get(1).unwrap();
+    let default_filename = format!("{}.svg", input);
+    let save_to = args.get(2).unwrap_or(&default_filename);
+
+    let operations = parse(input);
+    let path_data = convert(&operations);
+    let document = generate_svg(path_data);
+    svg::save(save_to, &document).unwrap();
+}
